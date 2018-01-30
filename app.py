69557f791482c8fe4c7f501980da380b72b67be4
theFOng <alecsanf@usc.edu>
2018-01-30 12:01:04 -0800

    initial commit

---
+++ app.py
@@ -0,0 +1,34 @@
+import boto3
+import time
+import cPickle as pickle
+
+min = 60
+filename = "log.txt"
+
+class Message(object):
+	"""docstring for Message"""
+	def __init__(self, phoneNumber, message, timeObj):
+		self.phoneNumber = phoneNumber
+		self.message = message
+		self.timeObj = timeObj
+
+def checkSend():
+	timeObj = time.localtime(time.time())
+	for x in messageList:
+		if getattr(timeObj,"tm_hour") == x.timeObj["tm_hour"] and getattr(timeObj,"tm_min") == x.timeObj["tm_min"]:
+			client = boto3.client('sns')
+			response = client.publish(
+			    PhoneNumber=x.phoneNumber,
+			    Message=x.message,
+			    MessageStructure='',
+			    MessageAttributes={}
+			)
+			fh = open(filename, "a")
+			fh.writelines([str(response),str(timeObj),str(vars(x)),"\n"])
+			fh.close	
+
+messageList = pickle.load( open( "save.p", "rb" ))	
+
+while True:
+	checkSend()
+	time.sleep(min)
---
+++ test.py
@@ -0,0 +1,26 @@
+import boto3
+import time
+import cPickle as pickle
+
+class Message(object):
+	"""docstring for Message"""
+	def __init__(self, phoneNumber, message, timeObj):
+		self.phoneNumber = phoneNumber
+		self.message = message
+		self.timeObj = timeObj
+
+m = Message("+18584650988","*message here*", {"tm_hour":18,"tm_min":55})
+
+mList = [m]
+
+pickle.dump( mList, open( "save.p", "wb" ) )
+
+# client = boto3.client('sns')
+# response = client.publish(
+#     PhoneNumber='+19494859948',
+#     Message='Test Py',
+#     MessageStructure='',
+#     MessageAttributes={}
+# )
+
+
