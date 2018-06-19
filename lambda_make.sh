514ea88a2deb7f847dafec9c85267bfa542ff705
Alec Fong <alecsanf@usc.edu>
2018-06-18 23:48:30 -0700

    Update lambda_make.sh

--- lambda_make.sh
+++ lambda_make.sh
@@ -2,7 +2,7 @@ cd ~/GoogleDrive/WorkStation/PowerNotify/
 rm -rf lambda_zip/
 mkdir lambda_zip/
 cp -r env/lib/python3.6/site-packages/* lambda_zip/
-cp -r .aws lambda_zip0/
+cp -r .aws lambda_zip/
 cp app_lambda.py lambda_zip/app.py
 cp email_creds.py lambda_zip/
-cd lambda_zip0/
+cd lambda_zip/
